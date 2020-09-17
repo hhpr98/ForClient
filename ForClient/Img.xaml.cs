45d527205d5d1ac2d11b2b3055fe6352f618158e
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-09-17 17:43:02 +0700

    fix: layout

--- ForClient/Img.xaml.cs
+++ ForClient/Img.xaml.cs
@@ -26,31 +26,11 @@ namespace ForClient
             InitializeComponent();
         }
 
-        /*
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            //var path = "..\\..\\Image\\Capture.jpg";
-            //if (File.Exists(path))
-            //{
-            //    MessageBox.Show("File is exists");
-            //}
-            //else
-            //{
-            //    MessageBox.Show("File does not exists");
-            //}
-
-            var path = "ms-appx:///Image/Capture.jpg";
-            BitmapImage bm = new BitmapImage(new Uri(path, UriKind.Relative));
-            
-            img.Source = bm;
-        }
-        */
-
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dlg = new OpenFileDialog();
             dlg.InitialDirectory = @"C:\Git\ForClient\ForClient\Image";
-            dlg.Filter = "Image files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+            dlg.Filter = "PNG files (*.png)|*.png|JPG files (*.jpg)|*.jpg|All Files (*.*)|*.*";
             dlg.RestoreDirectory = true;
 
             if (dlg.ShowDialog() == true)
@@ -61,7 +41,7 @@ namespace ForClient
                 bitmap.BeginInit();
                 bitmap.UriSource = new Uri(selectedFileName);
                 bitmap.EndInit();
-                ImageViewer1.Source = bitmap;
+                ImageViewer.Source = bitmap;
             }
         }
     }
